cb1c8f7bb1bf16332b0fddfc1931e01e00b0de2d
Nathan.fooo <86001920+appflowy@users.noreply.github.com>
2025-11-27 16:44:46 +0800

    fix: do not show template for selfhosting (#180)

--- src/components/app/SideBarBottom.tsx
+++ src/components/app/SideBarBottom.tsx
@@ -1,28 +1,33 @@
 import { IconButton, Tooltip } from '@mui/material';
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 
 import { ReactComponent as TrashIcon } from '@/assets/icons/delete.svg';
 import { ReactComponent as TemplateIcon } from '@/assets/icons/template.svg';
 import { QuickNote } from '@/components/quick-note';
+import { isOfficialHost } from '@/utils/subscription';
 
 function SideBarBottom() {
   const { t } = useTranslation();
   const navigate = useNavigate();
+  const isOfficial = useMemo(() => isOfficialHost(), []);
 
   return (
     <div className={'sticky bottom-0 bg-surface-container-layer-00 px-4'}>
       <div className={'flex items-center  justify-around gap-1 border-t border-border-primary py-4'}>
-        <Tooltip title={t('template.label')}>
-          <IconButton
-            size={'small'}
-            onClick={() => {
-              window.open(`${window.location.origin}/templates`, '_blank');
-            }}
-          >
-            <TemplateIcon />
-          </IconButton>
-        </Tooltip>
+        {isOfficial && (
+          <Tooltip title={t('template.label')}>
+            <IconButton
+              size={'small'}
+              onClick={() => {
+                window.open(`${window.location.origin}/templates`, '_blank');
+              }}
+            >
+              <TemplateIcon />
+            </IconButton>
+          </Tooltip>
+        )}
 
         <Tooltip title={t('trash.text')}>
           <IconButton
